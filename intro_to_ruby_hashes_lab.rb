3cfecb77d4cc01c887e93631738a9d81b228cb86
Chris Baum <christopher.baum@valpo.edu>
2021-01-15 02:38:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def new_hash
   hash = {}
 end
@@ -8,13 +10,12 @@ end
 
 def monopoly
 	monopoly = {}
-
 end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {railroads: {}}
-
+  binding.pry
 end
 
 def monopoly_with_third_tier
